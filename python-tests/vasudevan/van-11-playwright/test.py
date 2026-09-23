a7976f79b8468ae79ac62224b1eeb7928da8eec4
Vasudevan-c3174 <vasudevan.c@matodata.com>
2026-09-23 15:48:01 +0530

    Refactor employee management test script

--- python-tests/vasudevan/van-11-playwright/test.py
+++ python-tests/vasudevan/van-11-playwright/test.py
@@ -49,7 +49,8 @@ with sync_playwright() as p:
 )
     employee_id_input.fill(employee_id)
 
-    page.locator('input[type="file"]').set_input_files("V:/playwright/tests/Toolshop/kohli.jpg")
+    page.locator('input[type="file"]').set_input_files("playwright/python-tests/vasudevan/van-11-playwright/images
+/kohli.jpg")
     page.get_by_role("button", name="Save").click()
 
     # 4. Verify the employee is created successfully
