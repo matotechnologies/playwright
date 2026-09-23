2d9b2030f200ba3ddba47aa89f56177a1620e562
Vasudevan-c3174 <vasudevan.c@matodata.com>
2026-09-23 11:42:25 +0530

    Create test.spec.js

---
+++ JS-tests/vasudevan/task-test/tests/test.spec.js
@@ -0,0 +1,6 @@
+﻿const { test, expect } = require('@playwright/test');
+
+test('Toolshop frontend smoke test', async ({ page }) => {
+  await page.goto('http://localhost:4200');
+  await expect(page).toHaveTitle(/Practice Software Testing/i);
+});
