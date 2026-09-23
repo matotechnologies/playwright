bb2524b5e176aa2f06be6b88dadf9b698e8c9f83
Vasudevan-c3174 <vasudevan.c@matodata.com>
2026-09-23 12:13:57 +0530

    Delete python-tests/vasudevan/task-test directory

--- python-tests/vasudevan/task-test/test.py
+++
@@ -1,6 +0,0 @@
-from playwright.sync_api import Page, expect
-
-def test_toolshop_smoke(page: Page):
-    page.goto("http://localhost:4200")
-    expect(page).to_have_title("Practice Software Testing - Toolshop - v5.0")
-    
