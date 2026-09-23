de4199306982a6c54f12edb04aad353978abb235
Arundeepak-13 <arun.deepak@matodata.com>
2026-09-23 15:16:34 +0530

    Create playwright.config.js

---
+++ JS-tests/arundeepak/van-12-playwright/playwright.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+    testDir: './tests',
+
+    use: {
+        baseURL: 'http://localhost:8080',
+        headless: true,
+        screenshot: 'only-on-failure',
+        trace: 'on-first-retry'
+    },
+
+    reporter: 'html'
+});
