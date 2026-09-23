5be626c4e57256568785054a2f5aa12108900751
Vasudevan-c3174 <vasudevan.c@matodata.com>
2026-09-23 12:14:38 +0530

    Delete JS-tests/vasudevan/task-test directory

--- JS-tests/vasudevan/task-test/tests/test.spec.js
+++
@@ -1,7 +0,0 @@
-﻿const { test, expect } = require('@playwright/test');
-
-test('Toolshop frontend smoke test', async ({ page }) => {
-  await page.goto('http://localhost:4200');
-  await expect(page).toHaveTitle(/Practice Software Testing/i);
-  
-});
