a15edd353f4869a841772da733688361f623875b
Vasudevan-c3174 <vasudevan.c@matodata.com>
2026-09-23 11:45:11 +0530

    Add Playwright configuration file

---
+++ JS-tests/vasudevan/task-test/playwright.config.js
@@ -0,0 +1,10 @@
+﻿const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  use: {
+    baseURL: 'http://localhost:4200',
+    headless: true,
+  },
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+});
